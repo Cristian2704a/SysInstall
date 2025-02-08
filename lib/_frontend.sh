1d322f0b63317cbb445e2e10b4bdff5c75dd7cec
Lucas Saud <90074068+LucasSaud@users.noreply.github.com>
2025-02-08 17:40:59 -0300

    Update _frontend.sh

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -55,82 +55,70 @@ frontend_node_build() {
   printf "${WHITE} 💻 Compilando o código do frontend...${GRAY_LIGHT}"
   printf "\n\n"
   sleep 2
-sudo su - deploy <<EOF
+  
+  # Build
+  sudo su - deploy <<EOF
   cd /home/deploy/${instancia_add}/frontend
   npm run build
   rm -rf src
 EOF
+
+  # Ajustar permissões
+  sudo su - root <<EOF
+  chown -R deploy:deploy /home/deploy/${instancia_add}/
+  chmod -R 755 /home/deploy/${instancia_add}/frontend/build/
+  usermod -a -G deploy www-data
+EOF
+
   sleep 2
 }
 
-#######################################
-# sets frontend environment variables
-# Arguments:
-#   None
-#######################################
 frontend_set_env() {
   print_banner
   printf "${WHITE} 💻 Configurando variáveis de ambiente (frontend)...${GRAY_LIGHT}"
   printf "\n\n"
-
   sleep 2
-
-  # ensure idempotency
+  
   backend_url=$(echo "${backend_url/https:\/\/}")
   backend_url=${backend_url%%/*}
   backend_url=https://$backend_url
-
-  backend_hostname=$(echo "${backend_url/https:\/\/}")
 
 sudo su - deploy << EOF1
   cat <<-EOF2 > /home/deploy/${instancia_add}/frontend/.env
 REACT_APP_BACKEND_URL=${backend_url}
 REACT_APP_BACKEND_PROTOCOL=https
-REACT_APP_BACKEND_HOST=${backend_hostname}
+REACT_APP_BACKEND_HOST=${backend_url#*//}
 REACT_APP_BACKEND_PORT=443
 REACT_APP_HOURS_CLOSE_TICKETS_AUTO=24
 REACT_APP_LOCALE=pt-br
 REACT_APP_TIMEZONE=America/Sao_Paulo
-REACT_APP_FACEBOOK_APP_ID=
 EOF2
 EOF1
-
   sleep 2
-
 }
 
-#######################################
-# sets up nginx for frontend
-# Arguments:
-#   None
-#######################################
 frontend_nginx_setup() {
   print_banner
   printf "${WHITE} 💻 Configurando nginx (frontend)...${GRAY_LIGHT}"
   printf "\n\n"
-
   sleep 2
-
   frontend_hostname=$(echo "${frontend_url/https:\/\/}")
-
 sudo su - root << EOF
-
 cat > /etc/nginx/sites-available/${instancia_add}-frontend << 'END'
 server {
   server_name $frontend_hostname;
-  
   root /home/deploy/${instancia_add}/frontend/build;
   index index.html;
-
   location / {
-      try_files \$uri /index.html;
+    try_files \$uri /index.html;
+  }
+  location ~ /\.git {
+    deny all;
   }
 }
 END
-
 ln -s /etc/nginx/sites-available/${instancia_add}-frontend /etc/nginx/sites-enabled
 EOF
-
   sleep 2
 }
 
